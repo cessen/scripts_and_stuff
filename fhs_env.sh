7010671a3f998c27333df429ab611578735b55a7
Nathan Vegdahl <cessen@cessen.com>
2021-10-25 19:44:03 -0700

    Added a script for dropping into an fhs environment.

---
+++ fhs_env.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+FHS=`nix-build --no-out-link -E 'with import <nixpkgs> {};
+  buildFHSUserEnv {
+    name = "fhs";
+    targetPkgs = pkgs: with pkgs; [
+      mpv
+      zlib
+      glib
+      nss
+      nspr
+      freetype
+      expat
+      fontconfig
+      libglvnd
+      libGLU
+      libxkbcommon
+      systemd
+      pulseaudio
+      xlibs.libX11
+      xlibs.libXfixes
+      xlibs.libXi
+      xlibs.libXxf86vm
+      xlibs.libXrender
+      xlibs.libXcomposite
+      xlibs.libXdamage
+      xlibs.libXcursor
+      xlibs.libXrandr
+      xlibs.libxcb
+      xlibs.xcbutilkeysyms
+      xlibs.xcbutilwm
+      xlibs.xcbutilimage
+      xlibs.xcbutilrenderutil
+    ];
+  }
+'`
+
+"$FHS/bin/fhs"
