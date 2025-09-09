29870fa2b83b3862ae121c6416fbb0f7bdf56ac5
Nathan Vegdahl <cessen@cessen.com>
2025-09-09 00:28:41 -0700

    Make blender build env work again(?).

--- nix_dev_envs/blender/shell.nix
+++ nix_dev_envs/blender/shell.nix
@@ -1,103 +1,104 @@
 let
     # You can replace "unstable" with any channel name to use that channel.
-    pkgs = import <unstable> {};
+    pkgs = import <nixos> {};
     name = "blender_dev_env";
 
     # Shorthand.
-    pypkgs = pkgs.python310Packages;
+    pypkgs = pkgs.python311Packages;
 
-    # Custom build of freetype with brotli support added.
-    freetype_brotli = with pkgs; stdenv.mkDerivation rec {
-      name = "freetype_brotli";
-      pname = "freetype";
-      version = "2.12.1";
+    # # Custom build of freetype with brotli support added.
+    # freetype_brotli = with pkgs; stdenv.mkDerivation rec {
+    #   name = "freetype_brotli";
+    #   pname = "freetype";
+    #   version = "2.12.1";
 
-      src = fetchurl {
-        url = "mirror://savannah/${pname}/${pname}-${version}.tar.xz";
-        sha256 = "sha256-R2byAVfMTPDNKS+Av5F/ktHEObJDrDAY3r9rkUDEGn8=";
-      };
+    #   src = fetchurl {
+    #     url = "mirror://savannah/${pname}/${pname}-${version}.tar.xz";
+    #     sha256 = "sha256-R2byAVfMTPDNKS+Av5F/ktHEObJDrDAY3r9rkUDEGn8=";
+    #   };
 
-      propagatedBuildInputs = [ zlib bzip2 libpng brotli ]; # needed when linking against freetype
+    #   propagatedBuildInputs = [ zlib bzip2 libpng brotli ]; # needed when linking against freetype
 
-      # dependence on harfbuzz is looser than the reverse dependence
-      nativeBuildInputs = [ pkg-config which makeWrapper ]
-        # FreeType requires GNU Make, which is not part of stdenv on FreeBSD.
-        ++ lib.optional (!stdenv.isLinux) gnumake;
+    #   # dependence on harfbuzz is looser than the reverse dependence
+    #   nativeBuildInputs = [ pkg-config which makeWrapper ]
+    #     # FreeType requires GNU Make, which is not part of stdenv on FreeBSD.
+    #     ++ lib.optional (!stdenv.isLinux) gnumake;
 
-      # patches = [
-      #   ./enable-table-validation.patch
-      # ] ++ lib.optional useEncumberedCode ./enable-subpixel-rendering.patch;
+    #   # patches = [
+    #   #   ./enable-table-validation.patch
+    #   # ] ++ lib.optional useEncumberedCode ./enable-subpixel-rendering.patch;
 
-      outputs = [ "out" "dev" ];
+    #   outputs = [ "out" "dev" ];
 
-      configureFlags = [
-          "--bindir=$(dev)/bin"
-          "--enable-freetype-config"
-          "--with-brotli=yes"
-      ];
+    #   configureFlags = [
+    #       "--bindir=$(dev)/bin"
+    #       "--enable-freetype-config"
+    #       "--with-brotli=yes"
+    #   ];
 
-      # native compiler to generate building tool
-      CC_BUILD = "${buildPackages.stdenv.cc}/bin/cc";
+    #   # native compiler to generate building tool
+    #   CC_BUILD = "${buildPackages.stdenv.cc}/bin/cc";
 
-      # The asm for armel is written with the 'asm' keyword.
-      CFLAGS = lib.optionalString stdenv.isAarch32 "-std=gnu99";
+    #   # The asm for armel is written with the 'asm' keyword.
+    #   CFLAGS = lib.optionalString stdenv.isAarch32 "-std=gnu99";
 
-      enableParallelBuilding = true;
+    #   enableParallelBuilding = true;
 
-      doCheck = true;
+    #   doCheck = true;
 
-      postInstall = glib.flattenInclude + ''
-        substituteInPlace $dev/bin/freetype-config \
-          --replace ${buildPackages.pkg-config} ${pkgsHostHost.pkg-config}
+    #   postInstall = glib.flattenInclude + ''
+    #     substituteInPlace $dev/bin/freetype-config \
+    #       --replace ${buildPackages.pkg-config} ${pkgsHostHost.pkg-config}
 
-        wrapProgram "$dev/bin/freetype-config" \
-          --set PKG_CONFIG_PATH "$PKG_CONFIG_PATH:$dev/lib/pkgconfig"
-      '';
-    };
-    # Embree 4, which isn't packaged in nixos yet.
-    embree4 = with pkgs; stdenv.mkDerivation rec {
-      pname = "embree";
-      version = "4.0.1";
+    #     wrapProgram "$dev/bin/freetype-config" \
+    #       --set PKG_CONFIG_PATH "$PKG_CONFIG_PATH:$dev/lib/pkgconfig"
+    #   '';
+    # };
+    # # Embree 4, which isn't packaged in nixos yet.
+    # embree4 = with pkgs; stdenv.mkDerivation rec {
+    #   pname = "embree";
+    #   version = "4.0.1";
 
-      src = fetchFromGitHub {
-        owner = "embree";
-        repo = "embree";
-        rev = "v${version}";
-        sha256 = "sha256-PNiJPyQxFOpzKIuxeWje7o+Aus1fcWm+i3uVqmjaJ4g=";
-      };
+    #   src = fetchFromGitHub {
+    #     owner = "embree";
+    #     repo = "embree";
+    #     rev = "v${version}";
+    #     sha256 = "sha256-PNiJPyQxFOpzKIuxeWje7o+Aus1fcWm+i3uVqmjaJ4g=";
+    #   };
 
-      postPatch = ''
-        # Fix duplicate /nix/store/.../nix/store/.../ paths
-        sed -i "s|SET(EMBREE_ROOT_DIR .*)|set(EMBREE_ROOT_DIR $out)|" \
-          common/cmake/embree-config.cmake
-        sed -i "s|$""{EMBREE_ROOT_DIR}/||" common/cmake/embree-config.cmake
-        # substituteInPlace common/math/math.h --replace 'defined(__MACOSX__) && !defined(__INTEL_COMPILER)' 0
-        # substituteInPlace common/math/math.h --replace 'defined(__WIN32__) || defined(__FreeBSD__)' 'defined(__WIN32__) || defined(__FreeBSD__) || defined(__MACOSX__)'
-      '';
+    #   postPatch = ''
+    #     # Fix duplicate /nix/store/.../nix/store/.../ paths
+    #     sed -i "s|SET(EMBREE_ROOT_DIR .*)|set(EMBREE_ROOT_DIR $out)|" \
+    #       common/cmake/embree-config.cmake
+    #     sed -i "s|$""{EMBREE_ROOT_DIR}/||" common/cmake/embree-config.cmake
+    #     # substituteInPlace common/math/math.h --replace 'defined(__MACOSX__) && !defined(__INTEL_COMPILER)' 0
+    #     # substituteInPlace common/math/math.h --replace 'defined(__WIN32__) || defined(__FreeBSD__)' 'defined(__WIN32__) || defined(__FreeBSD__) || defined(__MACOSX__)'
+    #   '';
 
-      cmakeFlags = [
-        "-DEMBREE_TUTORIALS=OFF"
-        "-DEMBREE_RAY_MASK=ON"
-      ];
+    #   cmakeFlags = [
+    #     "-DEMBREE_TUTORIALS=OFF"
+    #     "-DEMBREE_RAY_MASK=ON"
+    #   ];
 
-      nativeBuildInputs = [ ispc pkg-config cmake ];
-      buildInputs = [ tbb glfw openimageio2 libjpeg libpng xorg.libX11 xorg.libpthreadstubs ]
-                    ++ lib.optionals stdenv.isDarwin [ glib ];
+    #   nativeBuildInputs = [ ispc pkg-config cmake ];
+    #   buildInputs = [ tbb glfw openimageio2 libjpeg libpng xorg.libX11 xorg.libpthreadstubs ]
+    #                 ++ lib.optionals stdenv.isDarwin [ glib ];
 
-      meta = with lib; {
-        description = "High performance ray tracing kernels from Intel";
-        homepage = "https://embree.github.io/";
-        maintainers = with maintainers; [ hodapp gebner ];
-        license = licenses.asl20;
-        platforms = platforms.unix;
-      };
-    };
-in pkgs.stdenv.mkDerivation {
+    #   meta = with lib; {
+    #     description = "High performance ray tracing kernels from Intel";
+    #     homepage = "https://embree.github.io/";
+    #     maintainers = with maintainers; [ hodapp gebner ];
+    #     license = licenses.asl20;
+    #     platforms = platforms.unix;
+    #   };
+    # };
+in pkgs.ccacheStdenv.mkDerivation {
     nativeBuildInputs = with pkgs; [
         # Custom items.
         pypkgs.numpy
         pypkgs.python
-        freetype_brotli
+        # freetype_brotli
+        freetype
 
         # Normal items.
         addOpenGLRunpath
@@ -107,14 +108,17 @@ in pkgs.stdenv.mkDerivation {
         c-blosc
         clang
         cmake
+        ccache
         # cudatoolkit
-        embree4
+        embree
         ffmpeg
         fftw
+        fftwFloat
         gettext
         glew
         gmp
         ilmbase
+        imath
         jemalloc
         libGL
         libGLU
@@ -133,8 +137,9 @@ in pkgs.stdenv.mkDerivation {
         openal
         opencollada
         opencolorio
-        openexr
-        openimagedenoise
+        # openexr
+        openexr_3
+        # openimagedenoise
         openimageio2
         openjpeg
         opensubdiv
@@ -145,6 +150,8 @@ in pkgs.stdenv.mkDerivation {
         SDL
         stdenv
         tbb
+        shaderc
+        vulkan-headers vulkan-loader
         xorg.libX11
         xorg.libXext
         xorg.libXi
