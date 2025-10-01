44ae133d9a4e325440c94d470512b767af5a5cf2
Nathan Vegdahl <cessen@cessen.com>
2025-10-01 09:20:02 +0900

    A working Blender dev environment (again) for NixOS.
    This time based on buildFHSEnv, so that we can just use the standard
    Blender build approach with pre-compiled libs, custom binaries, etc.

--- nix_dev_envs/blender/build_scripts/run-cmake-dev-lean.sh
+++ nix_dev_envs/blender/build_scripts/run-cmake-dev-lean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cmake \
   -G Ninja \
--- nix_dev_envs/blender/shell.nix
+++ nix_dev_envs/blender/shell.nix
@@ -1,173 +1,55 @@
-let
-    # You can replace "unstable" with any channel name to use that channel.
-    pkgs = import <nixos> {};
-    name = "blender_dev_env";
+{ pkgs ? import <nixpkgs> {} }:
 
-    # Shorthand.
-    pypkgs = pkgs.python311Packages;
+(pkgs.buildFHSEnv {
+  name = "Blender Dev Env";
 
-    # # Custom build of freetype with brotli support added.
-    # freetype_brotli = with pkgs; stdenv.mkDerivation rec {
-    #   name = "freetype_brotli";
-    #   pname = "freetype";
-    #   version = "2.12.1";
+  targetPkgs = pkgs: (with pkgs; [
+    # Note: use nix-locate to find packages that contain files (headers,
+    # shared objects, etc.) that are reported missing during building.
 
-    #   src = fetchurl {
-    #     url = "mirror://savannah/${pname}/${pname}-${version}.tar.xz";
-    #     sha256 = "sha256-R2byAVfMTPDNKS+Av5F/ktHEObJDrDAY3r9rkUDEGn8=";
-    #   };
+    cmake
+    clang
+    ccache
+    mold
+    pkg-config
 
-    #   propagatedBuildInputs = [ zlib bzip2 libpng brotli ]; # needed when linking against freetype
+    libGL
+    libGL.dev
+    pipewire
+    pipewire.dev
 
-    #   # dependence on harfbuzz is looser than the reverse dependence
-    #   nativeBuildInputs = [ pkg-config which makeWrapper ]
-    #     # FreeType requires GNU Make, which is not part of stdenv on FreeBSD.
-    #     ++ lib.optional (!stdenv.isLinux) gnumake;
+    # addDriverRunpath
+    # shaderc
+    # vulkan-headers vulkan-loader
 
-    #   # patches = [
-    #   #   ./enable-table-validation.patch
-    #   # ] ++ lib.optional useEncumberedCode ./enable-subpixel-rendering.patch;
+    xorg.libX11
+    xorg.libXext
+    xorg.libXi
+    xorg.libXrender
+    xorg.libXxf86vm
+    xorg.libXfixes
+    xorg.xorgproto
+    xorg.libSM
+    xorg.libICE
 
-    #   outputs = [ "out" "dev" ];
+    xorg.libX11.dev
+    xorg.libXext.dev
+    xorg.libXi.dev
+    xorg.libXrender.dev
+    xorg.libXxf86vm.dev
+    xorg.libXfixes.dev
+    xorg.xorgproto.out
+    xorg.libSM.out
+    xorg.libICE.out
 
-    #   configureFlags = [
-    #       "--bindir=$(dev)/bin"
-    #       "--enable-freetype-config"
-    #       "--with-brotli=yes"
-    #   ];
+    # For header files that are otherwise missing.
+    llvmPackages_20.libc.out
+  ]);
 
-    #   # native compiler to generate building tool
-    #   CC_BUILD = "${buildPackages.stdenv.cc}/bin/cc";
+  runScript = "fish";
 
-    #   # The asm for armel is written with the 'asm' keyword.
-    #   CFLAGS = lib.optionalString stdenv.isAarch32 "-std=gnu99";
-
-    #   enableParallelBuilding = true;
-
-    #   doCheck = true;
-
-    #   postInstall = glib.flattenInclude + ''
-    #     substituteInPlace $dev/bin/freetype-config \
-    #       --replace ${buildPackages.pkg-config} ${pkgsHostHost.pkg-config}
-
-    #     wrapProgram "$dev/bin/freetype-config" \
-    #       --set PKG_CONFIG_PATH "$PKG_CONFIG_PATH:$dev/lib/pkgconfig"
-    #   '';
-    # };
-    # # Embree 4, which isn't packaged in nixos yet.
-    # embree4 = with pkgs; stdenv.mkDerivation rec {
-    #   pname = "embree";
-    #   version = "4.0.1";
-
-    #   src = fetchFromGitHub {
-    #     owner = "embree";
-    #     repo = "embree";
-    #     rev = "v${version}";
-    #     sha256 = "sha256-PNiJPyQxFOpzKIuxeWje7o+Aus1fcWm+i3uVqmjaJ4g=";
-    #   };
-
-    #   postPatch = ''
-    #     # Fix duplicate /nix/store/.../nix/store/.../ paths
-    #     sed -i "s|SET(EMBREE_ROOT_DIR .*)|set(EMBREE_ROOT_DIR $out)|" \
-    #       common/cmake/embree-config.cmake
-    #     sed -i "s|$""{EMBREE_ROOT_DIR}/||" common/cmake/embree-config.cmake
-    #     # substituteInPlace common/math/math.h --replace 'defined(__MACOSX__) && !defined(__INTEL_COMPILER)' 0
-    #     # substituteInPlace common/math/math.h --replace 'defined(__WIN32__) || defined(__FreeBSD__)' 'defined(__WIN32__) || defined(__FreeBSD__) || defined(__MACOSX__)'
-    #   '';
-
-    #   cmakeFlags = [
-    #     "-DEMBREE_TUTORIALS=OFF"
-    #     "-DEMBREE_RAY_MASK=ON"
-    #   ];
-
-    #   nativeBuildInputs = [ ispc pkg-config cmake ];
-    #   buildInputs = [ tbb glfw openimageio2 libjpeg libpng xorg.libX11 xorg.libpthreadstubs ]
-    #                 ++ lib.optionals stdenv.isDarwin [ glib ];
-
-    #   meta = with lib; {
-    #     description = "High performance ray tracing kernels from Intel";
-    #     homepage = "https://embree.github.io/";
-    #     maintainers = with maintainers; [ hodapp gebner ];
-    #     license = licenses.asl20;
-    #     platforms = platforms.unix;
-    #   };
-    # };
-in pkgs.ccacheStdenv.mkDerivation {
-    nativeBuildInputs = with pkgs; [
-        # Custom items.
-        pypkgs.numpy
-        pypkgs.python
-        # freetype_brotli
-        freetype
-
-        # Normal items.
-        addOpenGLRunpath
-        alembic
-        boost
-        brotli
-        c-blosc
-        clang
-        cmake
-        ccache
-        # cudatoolkit
-        embree
-        ffmpeg
-        fftw
-        fftwFloat
-        gettext
-        glew
-        gmp
-        ilmbase
-        imath
-        jemalloc
-        libGL
-        libGLU
-        libepoxy
-        libharu
-        libjack2
-        libjpeg
-        libpng
-        libsamplerate
-        libsndfile
-        libtiff
-        libwebp
-        llvm
-        makeWrapper
-        ocl-icd
-        openal
-        opencollada
-        opencolorio
-        # openexr
-        openexr_3
-        # openimagedenoise
-        openimageio2
-        openjpeg
-        opensubdiv
-        openvdb
-        pkg-config
-        potrace
-        pugixml
-        SDL
-        stdenv
-        tbb
-        shaderc
-        vulkan-headers vulkan-loader
-        xorg.libX11
-        xorg.libXext
-        xorg.libXi
-        xorg.libXrender  
-        xorg.libXxf86vm
-        zlib
-        zstd
-    ];
-
-    inherit name;
-    shellHook = with pkgs; ''
-        export PYTHON_LIBRARY="${pypkgs.python.libPrefix}"
-        export PYTHON_LIBPATH="${pypkgs.python}/lib"
-        export PYTHON_INCLUDE_DIR="${pypkgs.python}/include/${pypkgs.python.libPrefix}"
-        export PYTHON_VERSION="${pypkgs.python.pythonVersion}"
-        export PYTHON_NUMPY_PATH="${pypkgs.numpy}/${pypkgs.python.sitePackages}"
-        export PYTHON_NUMPY_INCLUDE_DIRS="${pypkgs.numpy}/${pypkgs.python.sitePackages}/numpy/core/include"
-    '';
-}
+  profile=''
+    export NIX_ENFORCE_NO_NATIVE=0
+    export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/usr
+  '';
+}).env
